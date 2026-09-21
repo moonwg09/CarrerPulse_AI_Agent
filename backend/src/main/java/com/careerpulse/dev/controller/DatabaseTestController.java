eed9881f5960e3573b1ee4d9dc709c704aa5f610
moonwg <moonwg09@naver.com>
2026-09-21 17:01:18 +0900

    feat: 패키지 생성

--- backend/src/main/java/com/careerpulse/dev/controller/DatabaseTestController.java
+++ backend/src/main/java/com/careerpulse/dev/controller/DatabaseTestController.java
@@ -1,4 +1,4 @@
-package com.careerpulse.controller;
+package com.careerpulse.dev.controller;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
